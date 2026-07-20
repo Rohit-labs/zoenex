37a0563374eb8730d0ccf2e488262264b4ccc50e
Moresh-Mishra <moreshmishra@gmail.com>
2026-07-20 15:47:22 +0530

    retry

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 const BASE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://zoenexstudios.in"; 
-
+//commit changes
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
